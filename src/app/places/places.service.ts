ee084c78468493a4acca51c81ff54ee1b89b63af
Liane Stevenson <liane.stevenson@honeygroup.co.uk>
2022-01-24 10:22:00 +0000

    Booking app routing and foundation

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-underscore-dangle */
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+import { PlaceDetailPageModule } from './search/place-detail/place-detail.module';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Manhatten Mansion',
+      'In the heart of New York City.',
+      'https://media.timeout.com/images/105375857/image.jpg',
+      149.99
+    ),
+    new Place(
+      'p2',
+      'Roman Romance',
+      'Let Ancient Rome sweep you off your feet!',
+      'https://static.euronews.com/articles/stories/06/30/62/22/1440x810_cmsv2_5849e688-8914-5cef-bff5-2feae8cab76e-6306222.jpg',
+      189.99
+    ),
+    new Place(
+      'p3',
+      'Paris After Dark',
+      'A darker side of this magical city.',
+      'https://grittytravel.com/img/sliders/France-Paris-Notre-Dame-Cathedral-Dragon-Gargoyle.jpg',
+      99.99
+    ),
+  ];
+
+  constructor() { }
+
+  getPlaces() {
+    return [...this._places];
+  }
+
+  getPlace(placeId: string) {
+    const place = this._places.find(p => p.id === placeId);
+    return place;
+  }
+
+  getOffers() {
+    return [...this._places];
+  }
+}
